6f1e3d17ee52a28eea8d2fda393496e22c02a7e8
Thiago Aldrin Marques da Silva <thiago.amsilva@sp.senac.br>
2023-01-03 18:38:31 -0300

    Alteracao_001

--- Sintax/src/Main.java
+++ Sintax/src/Main.java
@@ -1,19 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//String[] vect = new String[]{"rosa","bob","Alex"};
+//
+//
+//for (int i =0;i<vect.length;i++){
+//    System.out.println(vect[i]);
+//}
+//    System.out.println("Hello world!");
+//
+////para cada obj da lista realize
+//for(String OOB : vect ){
+//    System.out.println(OOB);
+//}
+//
+//
+//}
 
-    String[] vect = new String[]{"rosa","bob","Alex"};
+
+List<String> lutadores = new ArrayList<>();
+    lutadores.add("Balrog");
+    lutadores.add("Honda");
+    lutadores.add("Juguernaaut");
+    lutadores.add("Blanca");
+    lutadores.add(2,"Riu");
+//a diferenca eh que aqui ele vsai pegar cada membro da lista individualmente
+//    for(String VarreLista : lutadores){
+//        System.out.println(VarreLista);
+//    }
 
 
-    for (int i =0;i<vect.length;i++){
-        System.out.println(vect[i]);
-    }
-        System.out.println("Hello world!");
+        //pode remover assim
+       // lutadores.remove(2);
 
-    //para cada obj da lista realize
-    for(String OOB : vect ){
-        System.out.println(OOB);
-    }
+//uma funcao para tirar da lista todos com aletra escolhida
+        lutadores.removeIf(x->x.charAt(0)=='J');
 
 
-    }
+
+
+        for(String VarreLista : lutadores){
+            System.out.println(VarreLista);
+        }
+
+        System.out.println(lutadores);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                if (mat[i][j] == x) {
+                    System.out.println("Position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (j < mat[i].length-1) {
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if (i < mat.length-1) {
+                        System.out.println("Down: " + mat[i+1][j]);
+                    }
+                }
+            }
+        }
+
+        sc.close();
+
 }
+
+
+
+
+}
+
+
+//listas
+
+
+
+
+
+
